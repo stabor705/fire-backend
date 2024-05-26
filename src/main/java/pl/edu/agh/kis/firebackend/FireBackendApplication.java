1ef76a03c10ea2c5f0b27cb3bb334f82e7daf914
Stanisław Borowy <stabor@startmail.com>
2024-05-26 13:28:14 +0200

    initial commit

---
+++ src/main/java/pl/edu/agh/kis/firebackend/FireBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.kis.firebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireBackendApplication.class, args);
+	}
+
+}
