8478c61053b5c72f6956669aa4fab9e704d07b79
dubielel <mdubielel@gmail.com>
2024-06-25 23:43:44 +0200

    Fix RabbitMQ queue names

--- src/main/java/pl/edu/agh/kis/firebackend/configuration/DeclaredQueues.java
+++ src/main/java/pl/edu/agh/kis/firebackend/configuration/DeclaredQueues.java
@@ -16,46 +16,46 @@ public class DeclaredQueues {
 
     @Bean
     Flux<EvFireBrigade> fireBrigadeUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.fireBrigade", EvFireBrigade.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("fireBrigade", EvFireBrigade.class));
     }
 
     @Bean
     Flux<ForesterPatrol> foresterPatrolUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.foresterPatrol", ForesterPatrol.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("foresterPatrol", ForesterPatrol.class));
     }
 
     @Bean
     Flux<EvWindSpeedSensor> windSpeedSensorUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.windSpeed", EvWindSpeedSensor.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("windSpeed", EvWindSpeedSensor.class));
     }
 
     @Bean
     Flux<EvTempAndAirHumiditySensor> tempAndAirHumiditySensorUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.tempAndAirHumidity", EvTempAndAirHumiditySensor.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("tempAndAirHumidity", EvTempAndAirHumiditySensor.class));
     }
 
     @Bean
     Flux<EvWindDirectionSensor> windDirectionSensorUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.windDirection", EvWindDirectionSensor.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("windDirection", EvWindDirectionSensor.class));
     }
 
     @Bean
     Flux<EvLitterMoistureSensor> litterMoistureSensorUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.litterMoisture", EvLitterMoistureSensor.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("litterMoisture", EvLitterMoistureSensor.class));
     }
 
     @Bean
     Flux<EvCO2Sensor> co2SensorUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.co2", EvCO2Sensor.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("co2", EvCO2Sensor.class));
     }
 
     @Bean
     Flux<EvPM25ConcentrationSensor> pm25ConcentrationSensorUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.pm25", EvPM25ConcentrationSensor.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("pm25", EvPM25ConcentrationSensor.class));
     }
 
     @Bean
     Flux<EvCamera> cameraUpdates() {
-        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("updates.camera", EvCamera.class));
+        return stateUpdatesService.createUpdatesFlux(new UpdatesQueue<>("camera", EvCamera.class));
     }
 }
